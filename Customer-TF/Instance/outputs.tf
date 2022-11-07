758d6c484693c06da25a7d56ec97433339692043
sadanandhegde <sadanandhegde@users.noreply.github.com>
2022-11-07 11:50:24 -0500

    Update outputs.tf

--- Customer-TF/Instance/outputs.tf
+++ Customer-TF/Instance/outputs.tf
@@ -1,6 +1,6 @@
 
 output "public_dns" {
-  value = aws_instance.myapp.public #something not sure??
+  value = aws_instance.myapp.public_dns #something not sure??
 
   # hint: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
 }
